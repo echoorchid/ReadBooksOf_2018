71a2ecd37bd8d3c4be7227ff767be7a81382bfac
echoorchid <1150821146@qq.com>
2018-03-04 21:37:04 +0800

    edit include path

--- test_vscode.cpp
+++ test_vscode.cpp
@@ -1,9 +1,11 @@
+#include <stdio.h>
 #include <iostream>
 using namespace std;
-int main
+int main()
 {
 	int a = 1;
 	int b = 2;
-	int c = a + b
-	cout << "a + b = " << c <<endl;
+	int c = a + b;
+	printf("a + b = %d", c);
+	return 0;
 }
